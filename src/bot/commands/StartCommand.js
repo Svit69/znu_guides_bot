a275b3f360db6f1e045601099cc7eda919bf3fde
Svit69 <ol.svitenkov@gmail.com>
2025-10-16 11:34:09 +0500

    feat: update consent message

--- src/bot/commands/StartCommand.js
+++ src/bot/commands/StartCommand.js
@@ -14,6 +14,19 @@ export class StartCommand extends CommandHandler {
    * @returns {Promise<void>}
    */
   async execute(ctx) {
-    await ctx.reply('Тест');
+    const message = [
+      'Перед тем как мы поделимся полезными материалами, прошу подтвердить своё согласие на получение',
+      '<a href="https://mantsova.tilda.ws/soglasie">информационных и маркетинговых сообщений</a> от нас,',
+      'а также на обработку персональных данных в соответствии с',
+      '<a href="https://zagorodom96.ru/privacy">Политикой конфиденциальности</a> и',
+      '<a href="https://mantsova.tilda.ws/oferta">Договором оферты</a>.',
+      '',
+      'Нажимая /start, вы подтверждаете своё согласие.'
+    ].join('\n');
+
+    await ctx.reply(message, {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true
+    });
   }
 }
